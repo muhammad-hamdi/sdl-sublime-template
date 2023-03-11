d9329d9c4f56a2c5ce6a0665612607fdebd91e0e
Muhammad Hamdi <prime.energy7@gmail.com>
2023-03-11 23:21:13 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char* args[])
+{
+	std::cout << "Test" << std::endl;
+	std::cin.get();
+	return 0;
+}
